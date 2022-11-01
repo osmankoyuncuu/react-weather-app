24355d6c112c3385c474629a3aa818bd9da56735
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-11-01 23:22:13 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { ThemeProvider } from "styled-components";
+import AppRouter from "./router/AppRouter";
+import { GlobalStyles } from "./Styles/Global.styled";
+
+const theme = {
+  color: {
+    first: "white",
+  },
+  responsive: {
+    small: "576px",
+    medium: "768px",
+    large: "992px",
+  },
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppRouter />
+    </ThemeProvider>
+  );
 };
 
 export default App;
